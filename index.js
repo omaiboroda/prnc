9c7903c502741ec579ab3bb5cfaeb1e09778a0e5
Oles Maiboroda <oles.maiboroda@gmail.com>
2019-10-20 18:09:49 +0200

    Init

---
+++ index.js
@@ -0,0 +1,24 @@
+/**
+ * This is the main entrypoint to your Probot app
+ * @param {import('probot').Application} app
+ */
+module.exports = app => {
+  // Your code here
+  app.log('Yay, the app was loaded!')
+
+  app.on('pull_request.opened', async context => {
+    const config = await context.config('pr-number-commenter.yml')
+    
+    const replacedMessage = config.message.replace(/\[\[PR\]\]/g, context.issue().number)
+
+    const issueComment = context.issue({ body: replacedMessage })
+    
+    return context.github.issues.createComment(issueComment)
+  })
+
+  // For more information on building apps:
+  // https://probot.github.io/docs/
+
+  // To get your app running against GitHub, see:
+  // https://probot.github.io/docs/development/
+}
